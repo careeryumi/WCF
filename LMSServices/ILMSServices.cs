d0ccf3d766a496cabc0f59601a13abf84f9487a1
Yumi <55708675+careeryumi@users.noreply.github.com>
2021-12-02 12:57:02 -0500

    Add files via upload

---
+++ LMSServices/ILMSServices.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+//Self hosting
+//VS Admin
+//Create Project --> Class Library(.NET Framework) --> LMSServices_Sec1
+//Delete the class
+//Add new item -> WCF services -- LMSSErvices_Sec1
+//ILMSSevices_Sect1 --> string GetMessage(string StudentName);
+//Implement this method in the class
+//Delete the app.config
+//We need to host the LMSServices
+//We created a new console application, name HOSTLMSServices
+//Refrence of System.ServiceModel, project --> LMSService_Sec1
+//Build the solution
+
+namespace LMSServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ILMSServices" in both code and config file together.
+    [ServiceContract]
+    public interface ILMSServices
+    {
+        [OperationContract]
+        string GetMessage(string StudentName);
+
+        [OperationContract]
+        string Hello();
+
+
+
+
+
+        [OperationContract]
+        List<Genre> GetAllGenre();
+
+        [OperationContract]
+        Genre GetGenre(int id);
+
+        [OperationContract]
+        int DeleteGenre(int id);
+
+        [OperationContract]
+        int CreateGenre(Genre genrePara);
+
+        [OperationContract]
+        int EditGenre(Genre genrePara);
+
+
+
+
+
+        [OperationContract]
+        List<Music> GetAllMusic();
+
+        [OperationContract]
+        Music GetMusic(int id);
+
+        [OperationContract]
+        int DeleteMusic(int id);
+
+        [OperationContract]
+        int CreateMusic(Music musicPara);
+
+        [OperationContract]
+        int EditMusic(Music musicPara);
+
+
+    }
+}
---
+++ LMSServices/LMSServices.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LMSServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LMSServices" in both code and config file together.
+    public class LMSServices : ILMSServices
+    {
+        public string GetMessage(string StudentName)
+        {
+            return "The name of the Student is " + StudentName;
+        }
+
+        public string Hello()
+        {
+            return "Hello world";
+        }
+
+
+        public List<Genre> GetAllGenre()
+        {
+
+            SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Genre", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Genre> genres = new List<Genre>();
+            while (reader.Read())
+            {
+                Genre genre = new Genre();
+                genre.GenreID = int.Parse(reader["GenreId"].ToString());
+                genre.GenreName = reader["GenreName"].ToString();
+
+                genres.Add(genre);
+            }
+
+            con.Close();
+            return genres;
+        }
+
+
+        //Used for detail and delete carrying id
+        public Genre GetGenre(int id)
+        {
+            SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT GenreID, GenreName FROM Genre WHERE GenreID = @GenreID", con);
+            cmd.Parameters.AddWithValue("@GenreID", id);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Genre genre = new Genre();
+
+            if (reader.Read())
+            {
+                genre.GenreID = int.Parse(reader["GenreID"].ToString());
+                genre.GenreName = reader["GenreName"].ToString();
+            }
+
+            con.Close();
+            return genre;
+        }
+
+
+
+        public int DeleteGenre(int id)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Genre WHERE GenreID = @GenreID", con);
+                cmd.Parameters.AddWithValue("@GenreID", id);
+
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public int CreateGenre(Genre genrePara)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+                con.Open();
+                //SqlCommand cmd = new SqlCommand("INSERT INTO Genre (GenreID, GenreName) VALUES (@GenreID, @GenreName) ", con);
+                //cmd.Parameters.AddWithValue("@GenreID", genrePara.GenreID);
+                //cmd.Parameters.AddWithValue("@GenreName", genrePara.GenreName);
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO Genre (GenreName) VALUES (@GenreName) ", con);
+                cmd.Parameters.AddWithValue("@GenreName", genrePara.GenreName);
+
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+
+        public int EditGenre(Genre genrePara)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Genre SET GenreName = @GenreName WHERE GenreID = @GenreID ", con);
+                cmd.Parameters.AddWithValue("@GenreID", genrePara.GenreID);
+                cmd.Parameters.AddWithValue("@GenreName", genrePara.GenreName);
+
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+
+
+        public List<Music> GetAllMusic()
+        {
+
+            SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT mu.MusicID, mu.MusicName, ge.GenreID AS genreID, ge.GenreName AS genreName FROM Music mu INNER JOIN Genre ge On mu.GenreID = ge.GenreID", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            List<Music> musics = new List<Music>();
+            while (reader.Read())
+            {
+                Music music = new Music();
+                music.MusicID = int.Parse(reader["MusicID"].ToString());
+                music.MusicName = reader["MusicName"].ToString();
+
+                Genre genre = new Genre();
+                genre.GenreID = int.Parse(reader["genreID"].ToString());
+                genre.GenreName = reader["genreName"].ToString();
+                music.Genre = genre;
+
+                musics.Add(music);
+            }
+
+            con.Close();
+            return musics;
+        }
+
+
+        //Used for detail and delete carrying id
+        public Music GetMusic(int id)
+        {
+            SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT MusicID, MusicName FROM Music WHERE MusicID = @MusicID", con);
+            cmd.Parameters.AddWithValue("@MusicID", id);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Music music = new Music();
+
+            if (reader.Read())
+            {
+                music.MusicID = int.Parse(reader["MusicID"].ToString());
+                music.MusicName = reader["MusicName"].ToString();
+            }
+
+            con.Close();
+            return music;
+        }
+
+
+
+        public int DeleteMusic(int id)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Music WHERE MusicID = @MusicID", con);
+                cmd.Parameters.AddWithValue("@MusicID", id);
+
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public int CreateMusic(Music musicPara)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Music (MusicName, GenreID) VALUES (@MusicName, @GenreID) ", con);
+                cmd.Parameters.AddWithValue("@MusicName", musicPara.MusicName);
+                cmd.Parameters.AddWithValue("@GenreID", musicPara.GenreID);
+
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
+
+        public int EditMusic(Music musicPara)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=.;Database=MusicDB;Trusted_Connection=True;MultipleActiveResultSets=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Music SET MusicName = @MusicName, GenreID = @GenreID WHERE MusicID = @MusicID ", con);
+                //SqlCommand cmd = new SqlCommand("UPDATE Music SET MusicName = @MusicName WHERE MusicID = @MusicID ", con);
+                cmd.Parameters.AddWithValue("@MusicID", musicPara.MusicID);
+                cmd.Parameters.AddWithValue("@MusicName", musicPara.MusicName);
+                cmd.Parameters.AddWithValue("@GenreID", musicPara.GenreID);
+
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+
+}
